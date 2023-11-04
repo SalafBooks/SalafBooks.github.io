2d0af3a14ade1ac94f6303737b714389b5866a81
Qazaroth <65128305+Qazaroth@users.noreply.github.com>
2023-11-04 22:26:03 +0800

    Hopefully this fixes...

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,3 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-function myFunction(){
-    let input = document.getElementById("myInput");
-    let filter = input.value.toLowerCase();
-    let itemlists = document.getElementsByTagName("div");
-
-    for (let i = 0; i < itemlists.length; i++){
-        let item = itemlists[i];
-        let id = item.getAttribute("data-value");
-        let h5 = item.getElementsByTagName("h5");
-        let h9 = item.getElementsByTagName("h9");
-
-        if (h5.length > 0 || h9.length > 0){
-            h5 = h5[0];
-            h9 = h9[0];
-            if (id != null || id){
-                if (id == filter || h5.innerHTML.toLowerCase().includes(filter) || h9.innerHTML.toLowerCase().includes(filter)){
-                    itemlists[i].style.display = "";
-                }
-                else{
-                    itemlists[i].style.display = "none";
-                }
-            }else{
-                itemlists[i].style.display = "";
-            }
-        }else{
-            itemlists[i].style.display = "";
-        }
-    }
-}
